f7e0a498e9fb53cd31a2e321b39d86437e1c9b29
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2024-03-15 21:48:28 +0330

    feat last article: add button all articles.

--- components/last-articles/index.js
+++ components/last-articles/index.js
@@ -1,10 +1,10 @@
 import { supabase } from "@/app/db/supabase";
 import Card from "../UI/card";
 import Container from "../UI/container";
-
+import Link from "next/link"
 export default async function LastArticles() {
     const fetchArticles = async () => {
-        const res = await supabase.from("weblog").select("id,title,abstract,updated_at,imageArticle").eq("published", true).order("created_at", {ascending: false})
+        const res = await supabase.from("weblog").select("id,title,abstract,updated_at,imageArticle").eq("published", true).order("created_at", { ascending: false }).limit(3);
         return res;
     };
     const data = (await fetchArticles()).data;
@@ -12,7 +12,12 @@ export default async function LastArticles() {
         <div className="mb-8">
             <Container>
                 <div>
-                    <h2 className="text-2xl mb-4">آخرین مقالات</h2>
+                    <div className="flex justify-between">
+                        <h2 className="text-2xl mb-4">آخرین مقالات</h2>
+                        <Link href="/blog">
+                            <span className="text-slate-500">مقالات بیشتر</span>
+                        </Link>
+                    </div>
                     <div className="grid grid-cols-1 gap-y-8 md:gap-8 md:grid-cols-2 lg:grid-cols-3">
                         {data.map((item) => (
                             <div key={item.id}>
