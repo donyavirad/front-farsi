23ee853c63bc620972f829b1e91ac40c1a01c951
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2024-03-17 10:06:10 +0330

    feat (single article): add tags to this page

--- app/blog/[articleId]/page.js
+++ app/blog/[articleId]/page.js
@@ -2,7 +2,7 @@ import Container from "@/components/UI/container";
 import { supabase } from "../../db/supabase";
 import { timeHandler } from "@/hooks/time";
 import LastArticleSidbar from "@/components/sidebar/LastArticleSidbar";
-
+import Link from "next/link";
 const fetchArticles = async (id) => {
     const res = await supabase.from("weblog").select("*").eq("id", id);
     return res;
@@ -10,7 +10,6 @@ const fetchArticles = async (id) => {
 
 export default async function Article(props) {
     const data = (await fetchArticles(props.params.articleId)).data[0];
-
     return (
         <div className="py-8">
             <Container>
@@ -25,17 +24,25 @@ export default async function Article(props) {
                                 <span>{timeHandler(data.updated_at)}</span>
                             </div>
                         </div>
-                        <div>
-                            <div
-                                className="text-gray"
-                                dangerouslySetInnerHTML={{
-                                    __html: data.content,
-                                }}
-                            ></div>
+                        <div
+                            className="text-black mb-4"
+                            dangerouslySetInnerHTML={{
+                                __html: data.content,
+                            }}
+                        ></div>
+                        <div className="bg-slate-100 rounded-lg px-4 py-2">
+                            <span className="block mb-2">برچسب ها:</span>
+                            <div className="flex flex-wrap space-x-4 space-x-reverse">
+                                {data.tags.map((item, id) => (
+                                    <Link key={id} className="bg-white px-2 py-1 rounded-lg" href={"/t/" + item}>
+                                        <span className=" text-slate-500 before:content-['•'] before:text-slate-500 before:ml-1 ">{item}</span>
+                                    </Link>
+                                ))}
+                            </div>
                         </div>
                     </div>
                     <div className="col-span-1">
-                        <LastArticleSidbar/>
+                        <LastArticleSidbar />
                     </div>
                 </div>
             </Container>
