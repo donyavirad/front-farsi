e2b98c99792823c8217496a401773cdb7eba000b
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2024-03-28 20:14:28 +0330

    feat layout index page: add metadata tags.

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,24 @@ import Navbar from "@/components/navbar";
 import "./globals.css";
 import Footer from "@/components/footer";
 export const metadata = {
+    metadataBase: new URL("https://front-farsi.netlify.app"),
     title: "سایت فرانت به فارسی | مقالات برنامه نویسی فرانت اند",
     description: "در این سایت می توانید مقالاتی درباره ی فرانت اند و برنامه نویسی بخوانید. و اگر سوالی دارید در قسمت پرسش و پاسخ بپرسید.",
+    generator: 'Next.js',
+    applicationName: 'فرانت فارسی',
+    referrer: 'origin-when-cross-origin',
+    keywords: ['Next.js', 'React', 'JavaScript', 'html', 'css', 'جاوااسکریت', 'وب', 'برنامه نویسی', 'فرانت اند','توسعه دهنده'],
+    authors: [{ name: 'حامد دنیوی راد' }, { name: 'حامد دنیوی راد', url: 'https://t.me/HamedDNR' }],
+    creator: 'حامد دنیوی راد',
+    publisher: 'حامد دنیوی راد',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    verification: {
+        google: "XJ-Hr8gOi4mIdPXl-vom41aMXFkAGyNzHcbTsuVCVJQ"
+    },
 };
 
 export const revalidate = 5;
