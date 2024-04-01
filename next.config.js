ce5380496219b2b49ddd10d4ce4bf54cc03c31d1
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2024-04-01 11:40:30 +0330

    feat next.config.js: increase secure project.

--- next.config.js
+++ next.config.js
@@ -8,7 +8,29 @@ const nextConfig = {
                 port: ""
             }
         ]
-    }
+    },
+    async headers() {
+        return [
+          {
+            // Matching all URLs
+            source: "/(.*)",
+            headers: [
+              {
+                key: "X-Content-Type-Options",
+                value: "nosniff",
+              },
+              {
+                key: "X-Frame-Options",
+                value: "SAMEORIGIN",
+              },
+              {
+                key: "X-XSS-Protection",
+                value: "1; mode=block",
+              },
+            ],
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
