68120b9885267a18164bea7048588a5cb1a3fd20
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2024-04-03 21:21:22 +0330

    fix article: add notFound to wrong url.

--- app/blog/[articleId]/page.js
+++ app/blog/[articleId]/page.js
@@ -3,58 +3,64 @@ import { supabase } from "../../db/supabase";
 import { timeHandler } from "@/hooks/time";
 import Link from "next/link";
 import Image from "next/image";
+import { notFound  } from 'next/navigation'
 import Content from "@/components/Content";
 import { Suspense } from "react";
 const fetchArticles = async (id) => {
     const res = await supabase.from("weblog").select("*").eq("id", id).eq("published", true);
-    console.log(res);
     return res;
 };
 
 export default async function Article(props) {
-    const data = (await fetchArticles(props.params.articleId)).data[0];
-    return (
-        <div className="py-8">
-            <Container>
-                <div className="w-full flex items-center justify-center ">
-                    <Suspense fallback={<p>در حال بارگذاری...</p>}>
-                        <div className="w-full space-y-8 lg:w-2/3">
-                            <div className="flex items-center justify-center">
-                                <Image
-                                    className="w-full rounded-2xl h-96 object-cover"
-                                    src={data.imageArticle.link}
-                                    width={800}
-                                    height={500}
-                                    sizes="(min-width: 1024px) 75vw, 100vw"
-                                    style={{
-                                        width: "100%",
-                                        height: "auto",
-                                    }}
-                                />
-                            </div>
-                            <div>
-                                <h1 className="text-3xl lg:text-4xl font-bold text-black mb-4">{data.title}</h1>
-                                <div className="flex space-x-2 space-x-reverse text-gray">
-                                    <span>{timeHandler(data.updated_at)}</span>
+    //console.log(props);
+    const response = await fetchArticles(props.params.articleId)
+    if(response.error) {
+        return notFound()
+    } else {
+        const data = response.data[0]
+        return (
+            <div className="py-8">
+                <Container>
+                    <div className="w-full flex items-center justify-center ">
+                        <Suspense fallback={<p>در حال بارگذاری...</p>}>
+                            <div className="w-full space-y-8 lg:w-2/3">
+                                <div className="flex items-center justify-center">
+                                    <Image
+                                        className="w-full rounded-2xl h-96 object-cover"
+                                        src={data.imageArticle.link}
+                                        width={800}
+                                        height={500}
+                                        sizes="(min-width: 1024px) 75vw, 100vw"
+                                        style={{
+                                            width: "100%",
+                                            height: "auto",
+                                        }}
+                                    />
+                                </div>
+                                <div>
+                                    <h1 className="text-3xl lg:text-4xl font-bold text-black mb-4">{data.title}</h1>
+                                    <div className="flex space-x-2 space-x-reverse text-gray">
+                                        <span>{timeHandler(data.updated_at)}</span>
+                                    </div>
+                                </div>
+                                <Content id={props.params.articleId} content={data.content} />
+                                <div className="bg-slate-100 rounded-lg px-4 py-2">
+                                    <span className="block mb-2">برچسب ها:</span>
+                                    <div className="flex flex-wrap space-x-4 space-x-reverse">
+                                        {data.tags.map((item, id) => (
+                                            <Link key={id} className="bg-white px-2 py-1 rounded-lg" href={"/t/" + item}>
+                                                <span className=" text-slate-500 before:content-['•'] before:text-slate-500 before:ml-1 ">{item}</span>
+                                            </Link>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                            <Content id={props.params.articleId} content={data.content} />
-                            <div className="bg-slate-100 rounded-lg px-4 py-2">
-                                <span className="block mb-2">برچسب ها:</span>
-                                <div className="flex flex-wrap space-x-4 space-x-reverse">
-                                    {data.tags.map((item, id) => (
-                                        <Link key={id} className="bg-white px-2 py-1 rounded-lg" href={"/t/" + item}>
-                                            <span className=" text-slate-500 before:content-['•'] before:text-slate-500 before:ml-1 ">{item}</span>
-                                        </Link>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </Suspense>
-                </div>
-            </Container>
-        </div>
-    );
+                        </Suspense>
+                    </div>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export async function generateMetadata({ params, searchParams }, parent) {
@@ -62,29 +68,33 @@ export async function generateMetadata({ params, searchParams }, parent) {
     const id = params.articleId;
 
     // fetch data
-    const res = (await supabase.from("weblog").select("*").eq("id", id)).data[0];
-
-    return {
-        title: res.title,
-        description: res.abstract,
-        openGraph: {
+    const resonse = await supabase.from("weblog").select("*").eq("id", id)
+    if(resonse.error) {
+        return notFound()
+    } else {
+        const res = resonse.data[0]
+        return {
             title: res.title,
-            description: res.description,
-            url: "/",
-            siteName: 'فرانت فارسی',
-            type: 'website',
-            images: [
-                {
-                    url: res.imageArticle.link,
-                    width: 400,
-                    height: 300,
-                },
-            ],
-        },
-        alternates: {
-            canonical: `/blog/${id}`,
-        },
-    };
+            description: res.abstract,
+            openGraph: {
+                title: res.title,
+                description: res.description,
+                url: "/",
+                siteName: 'فرانت فارسی',
+                type: 'website',
+                images: [
+                    {
+                        url: res.imageArticle.link,
+                        width: 400,
+                        height: 300,
+                    },
+                ],
+            },
+            alternates: {
+                canonical: `/blog/${id}`,
+            },
+        };
+    }
 }
 
 export const revalidate = 3600;
